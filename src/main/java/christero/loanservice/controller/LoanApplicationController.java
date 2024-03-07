3086a9289f8e4b682ae0ac32865b94bd13e1eace
christero <christer.olsen@gmail.com>
2024-03-07 20:29:13 +0100

    Add logging

--- src/main/java/christero/loanservice/controller/LoanApplicationController.java
+++ src/main/java/christero/loanservice/controller/LoanApplicationController.java
@@ -8,16 +8,21 @@ import christero.loanservice.mapper.LoanMapper;
 import christero.loanservice.model.LoanApplication;
 import christero.loanservice.service.LoanApplicationService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+
 
 @RestController
 @RequestMapping("/api/v1/application")
 public class LoanApplicationController {
 
     private final LoanApplicationService loanApplicationService;
+    private static final Logger logger = LoggerFactory.getLogger(LoanApplicationService.class);
 
     @Autowired
     public LoanApplicationController(LoanApplicationService loanApplicationService) {
@@ -26,16 +31,22 @@ public class LoanApplicationController {
 
     @PostMapping()
     public LoanApplicationReponseDTO applyForLoan(@Valid @RequestBody LoanApplicationRequestDTO dto) {
+        logger.info("Loan application received: {}", dto);
+
         LoanApplication loanApplication = loanApplicationService.applyForLoan(dto);
         return LoanMapper.INSTANCE.mapModelToResponseDto(loanApplication);
     }
 
     @GetMapping("/status/{applicationId}")
-    public ResponseEntity<LoanApplicationStatusResponseDTO> loanApplicationStatus(@PathVariable String applicationId) {
+    public ResponseEntity<LoanApplicationStatusResponseDTO> getStatus(@PathVariable String applicationId) {
         try {
             var loanApplication = loanApplicationService.getApplicationById(applicationId);
+
+            logger.info("Loan application status retrieved for ID {}", applicationId);
+
             return ResponseEntity.ok(LoanMapper.INSTANCE.mapModelToStatusResponseDto(loanApplication));
         } catch (LoanApplicationNotFoundException e) {
+            logger.error("Loan application not found for ID {}", applicationId);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
